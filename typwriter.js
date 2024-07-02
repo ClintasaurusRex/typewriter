e795242c86c138220e288dd5c0bf7d294436c570
Clint Hiles <clinthiles1992@gmail.com>
2024-07-02 14:24:43 -0700

    Making a typewriter

---
+++ typwriter.js
@@ -0,0 +1,13 @@
+
+const sentence = "hello there from lighthouse labs";
+let timer = 0;
+
+for (const letter of sentence) {
+  setTimeout(() => {
+    process.stdout.write(letter);
+  }, timer += 50);
+}
+
+setTimeout(() => {
+  process.stdout.write("\n");
+}, timer);
